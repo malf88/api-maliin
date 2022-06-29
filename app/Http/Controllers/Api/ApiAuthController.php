66ea47c6d558741691d8f729f776ece31d9dfc86
Marco Aurélio <malf88@gmail.com>
2022-06-28 23:34:07 -0300

    Mantido o login clássico por questões de retrocompatibilidade.

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 
 class ApiAuthController extends Controller
 {
@@ -40,9 +41,15 @@ class ApiAuthController extends Controller
         }
     }
 
+    /**
+     * @param Request $request
+     * @return string
+     * @deprecated
+     */
     public function token(Request $request){
         try {
-
+            if(config('app.env') == 'production')
+                throw new AccessDeniedException('Você não pode acessar essa URL');
             $user = Socialite::driver('google')->user();
 
             return $user->token;
@@ -66,6 +73,8 @@ class ApiAuthController extends Controller
      *
      */
     public function login(Request $request){
+        if(config('app.env') == 'production')
+            throw new AccessDeniedException('Você não pode acessar essa URL');
         return Socialite::driver('google')->redirect();
     }
     /**
@@ -112,7 +121,52 @@ class ApiAuthController extends Controller
      *
      */
     public function logout(Request $request){
-
         return $request->user()->tokens()->where('token', $request->user()->currentAccessToken()->token)->delete();
+    }
+
+    /**
+     * @OA\Post(
+     *     tags={"Login"},
+     *     summary="Realiza o login do usuário",
+     *     description="Realiza o login do usuário e retorna um token",
+     *     path="/token",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string"
+     *                 ),
+     *                 example={"email": "teste@teste.com", "password": "12345"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *          response="201",
+     *          description="Login aconteceu com sucesso",
+     *          @OA\JsonContent()
+     *      ),
+     * ),
+     * @deprecated
+     */
+
+    public function classicalLogin(Request $request){
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+        $user = User::where('email', $request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token');
+
+        return [
+            'token' => $token->plainTextToken,
+            'token_type' => 'Bearer',
+        ];
     }
 }

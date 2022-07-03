70ed8d9c630e8763bc8383df66deea00d5c0286a
Marco Aurélio <malf88@gmail.com>
2022-07-03 15:51:53 -0300

    Corrigido caminho do google redirect.

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT','http://localhost/api/auth/token'),
+        'redirect' => env('GOOGLE_REDIRECT','http://localhost/auth/token'),
     ],
 
 ];

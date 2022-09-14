655d0eea62107e42e24dcde3b44e5811025e3692
Marco Aurélio <malf88@gmail.com>
2022-09-14 20:44:28 -0300

    Configuração em ambiente de produção.

--- config/horizon.php
+++ config/horizon.php
@@ -185,6 +185,7 @@ return [
                 'maxProcesses' => 10,
                 'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
+                'balance' => 'simple',
             ],
         ],
 
